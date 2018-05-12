6c09e797267440781681a4552af63ca0897dec70
Hugo Campos <hugofcampos@gmail.com>
2018-05-12 01:23:04 -0300

    Add insert answers

---
+++ app/web/index.php
@@ -0,0 +1,41 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+        'driver'   => 'pdo_sqlite',
+        'path'     => __DIR__.'/app.db',
+    ),
+));
+
+$app->before(function (Request $request) use ($app){
+    $sql = "CREATE TABLE IF NOT EXISTS answers (id INTEGER PRIMARY KEY AUTOINCREMENT, user VARCHAR(255), question INT, answer INT)";
+    $post = $app['db']->executeUpdate($sql);
+
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
+        $request->request->replace(is_array($data) ? $data : array());
+    }
+});
+
+$app->post('/answers', function (Request $request) use ($app) {
+    $user = $request->request->get('user');
+    $question = $request->request->get('question');
+    $answer = $request->request->get('answer');
+
+    $sql = "INSERT INTO answers (user, question, answer) values (?,?,?)";
+    $app['db']->executeUpdate($sql, [$user, $question, $answer]);
+
+    return new JsonResponse(['id' => (int) $app['db']->lastInsertId()], 201);
+});
+
+$app->run();
